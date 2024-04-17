1d24662d8e9527aa4ad93d6a93c7b2008df4a460
anyway <anm.sleepalone@gmail.com>
2024-04-17 15:20:24 +0800

    更新命令行方式启动

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use setfn::set_fn;
+use std::process::CommandArgs;
 mod setfn;
 
 #[tauri::command]
@@ -9,10 +10,56 @@ fn toggle_switch( can_fn:bool ) -> String {
     return  set_fn(can_fn);
 }
 
+#[tauri::command]
+fn handle_command() {
+    let mut args = std::env::args();
+
+    // 跳过第一个参数,因为它是程序的名称
+    if let Some(_) = args.next() {
+        if let Some(arg) = args.next() {
+            match arg.as_str() {
+                "-m" | "--mode" => {
+                    if let Some(mode) = args.next() {
+                        match mode.as_str() {
+                            "lock" => {
+                                // 执行 "lock" 模式的逻辑
+                                println!("Locking the application...");
+                                set_fn(true);
+                            }
+                            "unlock" => {
+                                // 执行 "unlock" 模式的逻辑
+                                println!("Unlocking the application...");
+                                set_fn(false);
+                            }
+                            _ => {
+                                println!("Unknown mode: {}", mode);
+                            }
+                        }
+                    } else {
+                        println!("Missing mode argument");
+                    }
+                }
+                _ => {
+                    println!("Unknown argument: {}", arg);
+                }
+            }
+        }
+    }
+}
+
+
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![toggle_switch])
-        .run(tauri::generate_context!())
-        .expect("启动时发生异常!");
+    let args = std::env::args();
+        if args.len() > 1 {
+            // 如果有命令行参数,则不展示界面
+            println!("启动中");
+            handle_command()
+        } else {
+            // 如果没有命令行参数,则展示界面
+            tauri::Builder::default()
+            .invoke_handler(tauri::generate_handler![toggle_switch])
+            .run(tauri::generate_context!())
+            .expect("启动时发生异常!");
+        }
 }
